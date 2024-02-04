3b063212672e137ed12935f05e5cd315043b38fd
Lukas Ruegner <ruegnerlukas@gmail.com>
2024-02-04 20:54:10 +0100

    javadoc

--- schema-kenerator-serialization/src/main/kotlin/io/github/smiley4/schemakenerator/serialization/KotlinxSerializationTypeParserConfig.kt
+++ schema-kenerator-serialization/src/main/kotlin/io/github/smiley4/schemakenerator/serialization/KotlinxSerializationTypeParserConfig.kt
@@ -4,31 +4,70 @@ import io.github.smiley4.schemakenerator.core.parser.TypeParserConfig
 import kotlin.reflect.KClass
 
 /**
- * Configuration for parsing kotlin types
+ * Configuration for parsing kotlin types.
  */
 class KotlinxSerializationTypeParserConfig(
-    val customParser: CustomKotlinxSerializationTypeParser?,
-    val customParsers: Map<String, CustomKotlinxSerializationTypeParser>,
+    /**
+     * Whether to inline additional types or keep them separate in the context and reference them.
+     */
     val inline: Boolean,
+    /**
+     * An optional custom parser for all types, overwriting the default parser logic is required.
+     */
+    val customParser: CustomKotlinxSerializationTypeParser?,
+    /**
+     * Custom parsers for specific types, overwriting the default parser for the given type.
+     */
+    val customParsers: Map<String, CustomKotlinxSerializationTypeParser>,
 ) : TypeParserConfig()
 
 
+/**
+ * Builder for the [KotlinxSerializationTypeParserConfig]
+ */
 class KotlinxSerializationTypeParserConfigBuilder {
 
+    /**
+     * Whether to inline additional types or keep them separate in the context and reference them.
+     */
     var inline: Boolean = true
 
+
+    /**
+     * A custom parser for all types.
+     */
     var customParser: CustomKotlinxSerializationTypeParser? = null
 
+
+    /**
+     * The custom parsers for specific types.
+     */
     private val parsers = mutableMapOf<String, CustomKotlinxSerializationTypeParser>()
 
+
+    /**
+     * Register a custom parser for the given type.
+     * @param type the type to overwrite the default parsing behaviour for
+     * @param parser the custom parser for the given type
+     */
     fun registerParser(type: KClass<*>, parser: CustomKotlinxSerializationTypeParser) {
         type.qualifiedName?.also { registerParser(it, parser) }
     }
 
+
+    /**
+     * Register a custom parser for the given type.
+     * @param type the qualified name of the type to overwrite the default parsing behaviour for
+     * @param parser the custom parser for the given type
+     */
     fun registerParser(type: String, parser: CustomKotlinxSerializationTypeParser) {
         parsers[type] = parser
     }
 
+
+    /**
+     * Build the completed [KotlinxSerializationTypeParserConfig].
+     */
     fun build(): KotlinxSerializationTypeParserConfig {
         return KotlinxSerializationTypeParserConfig(
             customParser = customParser,
