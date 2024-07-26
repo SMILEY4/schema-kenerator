8e22d29820798d644622118faeac12cd81368caf
Lukas Ruegner <ruegnerlukas@gmail.com>
2024-07-26 16:04:25 +0200

    manual customization steps for swagger schema

--- schema-kenerator-test/src/test/kotlin/io/github/smiley4/schemakenerator/test/Test.kt
+++ schema-kenerator-test/src/test/kotlin/io/github/smiley4/schemakenerator/test/Test.kt
@@ -2,31 +2,96 @@ package io.github.smiley4.schemakenerator.test
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.github.smiley4.schemakenerator.core.connectSubTypes
-import io.github.smiley4.schemakenerator.core.handleNameAnnotation
-import io.github.smiley4.schemakenerator.jsonschema.compileReferencingRoot
-import io.github.smiley4.schemakenerator.jsonschema.generateJsonSchema
-import io.github.smiley4.schemakenerator.jsonschema.handleCoreAnnotations
-import io.github.smiley4.schemakenerator.reflection.collectSubTypes
-import io.github.smiley4.schemakenerator.reflection.processReflection
-import io.github.smiley4.schemakenerator.serialization.steps.KotlinxSerializationTypeProcessingStep
-import io.github.smiley4.schemakenerator.swagger.compileReferencingRoot
+import io.github.smiley4.schemakenerator.core.data.AnnotationData
+import io.github.smiley4.schemakenerator.core.data.BaseTypeData
+import io.github.smiley4.schemakenerator.core.data.Bundle
+import io.github.smiley4.schemakenerator.core.data.PrimitiveTypeData
+import io.github.smiley4.schemakenerator.core.data.TypeId
+import io.github.smiley4.schemakenerator.serialization.processKotlinxSerialization
+import io.github.smiley4.schemakenerator.swagger.compileInlining
+import io.github.smiley4.schemakenerator.swagger.customizeTypes
+import io.github.smiley4.schemakenerator.swagger.data.SwaggerSchema
 import io.github.smiley4.schemakenerator.swagger.generateSwaggerSchema
-import io.github.smiley4.schemakenerator.swagger.handleCoreAnnotations
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaCompileReferenceRootStep
-import io.github.smiley4.schemakenerator.swagger.steps.SwaggerSchemaGenerationStep
-import io.github.smiley4.schemakenerator.test.models.kotlinx.ClassDirectSelfReferencing
+import io.github.smiley4.schemakenerator.swagger.steps.buildTypeDataMap
+import io.github.smiley4.schemakenerator.test.models.kotlinx.ClassWithLocalDateTime
 import io.kotest.core.spec.style.StringSpec
+import java.time.LocalDateTime
 import kotlin.reflect.typeOf
 
 class Test : StringSpec({
 
-    "kotlinx processing infinite loop" {
+    "before" {
 
-        val result = typeOf<ClassDirectSelfReferencing>()
-            .let { KotlinxSerializationTypeProcessingStep().process(it) }
-            .let { SwaggerSchemaGenerationStep().generate(it) }
-            .let { SwaggerSchemaCompileReferenceRootStep().compile(it) }
+        fun process(schema: SwaggerSchema, typeDataMap: Map<TypeId, BaseTypeData>) {
+            schema.typeData.annotations
+                .find { it.name == "swagger-format" } // find our custom annotation
+                ?.also { annotation ->
+                    schema.swagger.format = annotation.values["format"] as String // set the "format"-property of the swagger-schema
+                }
+        }
+
+        val result = typeOf<ClassWithLocalDateTime>()
+            .processKotlinxSerialization {
+                customProcessor<LocalDateTime> {
+                    PrimitiveTypeData(
+                        id = TypeId.build(String::class.qualifiedName!!),
+                        simpleName = String::class.simpleName!!,
+                        qualifiedName = String::class.qualifiedName!!,
+                        annotations = mutableListOf(
+                            AnnotationData(
+                                name = "swagger-format",
+                                values = mutableMapOf("format" to "date-time"),
+                                annotation = null
+                            )
+                        )
+                    )
+                }
+            }
+            .generateSwaggerSchema()
+            .let { bundle ->
+                val typeDataMap = bundle.buildTypeDataMap()
+                bundle.also { schema ->
+                    process(schema.data, typeDataMap)
+                    schema.supporting.forEach { process(it, typeDataMap) }
+                }
+            }
+            .compileInlining()
+
+
+        println(json.writeValueAsString(result.swagger))
+        result.componentSchemas.forEach { (name, schema) ->
+            println("$name: ${json.writeValueAsString(schema)}")
+        }
+    }
+
+
+    "after" {
+
+        val result = typeOf<ClassWithLocalDateTime>()
+            .processKotlinxSerialization {
+                customProcessor<LocalDateTime> {
+                    PrimitiveTypeData(
+                        id = TypeId.build(String::class.qualifiedName!!),
+                        simpleName = String::class.simpleName!!,
+                        qualifiedName = String::class.qualifiedName!!,
+                        annotations = mutableListOf(
+                            AnnotationData(
+                                name = "swagger-format",
+                                values = mutableMapOf("format" to "date-time"),
+                                annotation = null
+                            )
+                        )
+                    )
+                }
+            }
+            .generateSwaggerSchema()
+            .customizeTypes { type, schema ->
+                type.annotations.find { it.name == "swagger-format" }?.also { annotation ->
+                    schema.format = annotation.values["format"] as String
+                }
+            }
+            .compileInlining()
+
 
         println(json.writeValueAsString(result.swagger))
         result.componentSchemas.forEach { (name, schema) ->
@@ -36,6 +101,9 @@ class Test : StringSpec({
 
 }) {
     companion object {
+
         private val json = jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL).writerWithDefaultPrettyPrinter()!!
+
+
     }
 }
