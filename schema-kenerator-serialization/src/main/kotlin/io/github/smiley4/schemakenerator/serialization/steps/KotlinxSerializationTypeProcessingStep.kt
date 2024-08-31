7091df0be4d0eb2fae62a0e0890e7e33d80756bb
Lukas Ruegner <ruegnerlukas@gmail.com>
2024-08-31 11:31:45 +0200

    wip: kotlinx contextual

--- schema-kenerator-serialization/src/main/kotlin/io/github/smiley4/schemakenerator/serialization/steps/KotlinxSerializationTypeProcessingStep.kt
+++ schema-kenerator-serialization/src/main/kotlin/io/github/smiley4/schemakenerator/serialization/steps/KotlinxSerializationTypeProcessingStep.kt
@@ -27,9 +27,11 @@ import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.SerialKind
 import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.capturedKClass
 import kotlinx.serialization.descriptors.elementDescriptors
 import kotlinx.serialization.descriptors.elementNames
 import kotlinx.serialization.descriptors.nonNullOriginal
+import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializerOrNull
 import java.lang.reflect.Modifier
 import kotlin.reflect.KClass
@@ -45,6 +47,10 @@ class KotlinxSerializationTypeProcessingStep(
      * custom processors for given types that overwrite the default behaviour
      */
     private val customProcessors: List<Pair<KotlinxSerializationTypeMatcher, KotlinxSerializationCustomProcessor>> = emptyList(),
+    /**
+     * kotlinx serializers module from `Json { }.serializersModule` for support of contextual serializers
+     */
+    private val serializersModule: SerializersModule? = null,
     /**
      * redirect types to other types, i.e. when a type is found as a key, the corresponding type will be processed instead
      */
@@ -173,6 +179,14 @@ class KotlinxSerializationTypeProcessingStep(
             }
         if (customData != null) {
             return customData
+        }
+
+        // check contextual descriptors
+        val contextualByKClass = descriptor.capturedKClass?.let { serializersModule?.getContextual(it)?.descriptor }
+        if (contextualByKClass != null) {
+            // todo: for uuid-example: contextualByKClass is primitiveSerialDescriptor of kind "string", but there is no way to pass the "string" information along
+            //  -> idea: seperate "name"/id from "underlying type"
+            return parse(contextualByKClass, nullable, knownTypeData, processedDescriptors)
         }
 
         // parse descriptor
--- schema-kenerator-test/src/test/kotlin/io/github/smiley4/schemakenerator/test/_ManualTests.kt
+++ schema-kenerator-test/src/test/kotlin/io/github/smiley4/schemakenerator/test/_ManualTests.kt
@@ -1,94 +1,79 @@
+@file:UseSerializers(InstantSerializer::class, MyUUIDSerializer::class)
+@file:OptIn(ExperimentalSerializationApi::class)
 @file:Suppress("ClassName")
+
 
 package io.github.smiley4.schemakenerator.test
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.github.smiley4.schemakenerator.core.annotations.Format
-import io.github.smiley4.schemakenerator.core.annotations.Title
-import io.github.smiley4.schemakenerator.core.annotations.Type
+import io.github.smiley4.schemakenerator.jsonschema.compileInlining
+import io.github.smiley4.schemakenerator.jsonschema.data.TitleType
+import io.github.smiley4.schemakenerator.jsonschema.generateJsonSchema
+import io.github.smiley4.schemakenerator.jsonschema.withTitle
+import io.github.smiley4.schemakenerator.reflection.processReflection
+import io.github.smiley4.schemakenerator.serialization.processKotlinxSerialization
+import io.github.smiley4.schemakenerator.swagger.compileInlining
 import io.github.smiley4.schemakenerator.swagger.data.CompiledSwaggerSchema
 import io.kotest.core.spec.style.StringSpec
+import io.swagger.v3.core.util.Json31
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import java.time.Instant
+import java.util.UUID
+import kotlin.reflect.typeOf
 
 /**
  * internal / manual tests only
  */
 class _ManualTests : StringSpec({
-//
-//    "with default ktor-openapi generator" {
-//        val result = typeOf<MyInstantClass>()
-//            .collectSubTypes()
-//            .processReflection()
-//            .addMissingSupertypeSubtypeRelations()
-//            .handleNameAnnotation()
-//            .generateSwaggerSchema()
-//            .handleCoreAnnotations()
-//            .withTitle(TitleType.SIMPLE)
-//            .compileReferencingRoot()
-//            .asPrintable()
-//            .let { json.writeValueAsString(it) }
-//
-//        println(result)
-//    }
-//
-//    "with default ktor-openapi generator (fix)" {
-//        val result = typeOf<MyInstantClass>()
-//            .collectSubTypes()
-//            .processReflection()
-//            .addMissingSupertypeSubtypeRelations()
-//            .handleNameAnnotation()
-//            .generateSwaggerSchema()
-//            .handleCoreAnnotations()
-//            .withTitle(TitleType.SIMPLE)
-//            .mergePropertyAttributesIntoType()
-//            .compileReferencingRoot()
-//            .asPrintable()
-//            .let { json.writeValueAsString(it) }
-//
-//        println(result)
-//    }
-//
-//    "with custom generator" {
-//        val result = typeOf<MyInstantClass>()
-//            .collectSubTypes()
-//            .processReflection {
-//                customProcessor<DummyInstant> {
-//                    PrimitiveTypeData(
-//                        id = TypeId.build(DummyInstant::class.qualifiedName!!),
-//                        simpleName = DummyInstant::class.simpleName!!,
-//                        qualifiedName = DummyInstant::class.qualifiedName!!,
-//                    )
-//                }
-//            }
-//            .addMissingSupertypeSubtypeRelations()
-//            .handleNameAnnotation()
-//            .generateSwaggerSchema()
-//            .handleCoreAnnotations()
-//            .withTitle(TitleType.SIMPLE)
-//            .let { SwaggerMergePropertyAttributesStep().process(it) }
-//            .compileReferencingRoot()
-//            .asPrintable()
-//            .let { json.writeValueAsString(it) }
-//
-//        println(result)
-//    }
+
+    "test" {
+
+        val JSON = Json {
+            serializersModule = SerializersModule {
+                contextual(UUID::class, MyUUIDSerializer)
+            }
+        }
+
+        println(JSON.encodeToString(MyData(UUID.randomUUID())))
+        println()
+
+        val result = typeOf<MyData>()
+            .processKotlinxSerialization {
+                serializersModule = JSON.serializersModule
+            }
+            .also {
+                println(it)
+            }
+            .generateJsonSchema()
+            .withTitle(TitleType.SIMPLE)
+            .compileInlining()
+            .json
+            .prettyPrint()
+
+        println(result)
+    }
 
 }) {
     companion object {
 
         @Serializable
-        @Title("My title")
-        @Type("string")
-        data class MyInstantClass(
-            @Type("string")
-            @Format("date-time")
-            val time: DummyInstant
+        class MyData(
+            @Contextual
+            val myId: UUID
         )
-
-
-        @Serializable
-        class DummyInstant
 
         class SwaggerResult(
             val root: io.swagger.v3.oas.models.media.Schema<*>,
@@ -105,4 +90,18 @@ class _ManualTests : StringSpec({
         private val json = jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL).writerWithDefaultPrettyPrinter()!!
 
     }
+}
+
+
+
+object InstantSerializer : KSerializer<Instant> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("java.time.Instant", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: Instant) = encoder.encodeString(value.toString())
+    override fun deserialize(decoder: Decoder): Instant = Instant.parse(decoder.decodeString())
+}
+
+object MyUUIDSerializer : KSerializer<UUID> {
+    override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
+    override fun deserialize(decoder: Decoder): UUID = UUID.fromString(decoder.decodeString())
+    override fun serialize(encoder: Encoder, value: UUID) = encoder.encodeString(value.toString())
 }
